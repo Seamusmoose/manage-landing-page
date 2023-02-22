8284781b980634580fa7bf15d13a359028f5b809
Seamus OSullivan <info@seamuso.com>
2023-02-22 17:45:30 +0100

    refactor css

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -26,10 +26,9 @@ export const Carousel = (props: Props) => {
     <div className="flex f_col">
       <h2>What they’ve said</h2>
       <div className="flex f_center carousel">
-        
         <div id="avatarOne" className="carousel__item">
           <h3>Anisha Li </h3>
-          <p>
+          <p className="general_text">
             “Manage has supercharged our team’s workflow. The ability to
             maintain visibility on larger milestones at all times keeps everyone
             motivated.”
@@ -37,7 +36,7 @@ export const Carousel = (props: Props) => {
         </div>
         <div id="avatarTwo" className="carousel__item">
           <h3>Ali Bravo</h3>
-          <p>
+          <p className="general_text">
             “We have been able to cancel so many other subscriptions since using
             Manage. There is no more cross-channel confusion and everyone is
             much more focused.”
@@ -45,7 +44,7 @@ export const Carousel = (props: Props) => {
         </div>
         <div id="avatarThree" className="carousel__item">
           <h3>Richard Watts</h3>
-          <p>
+          <p className="general_text">
             “Manage allows us to provide structure and process. It keeps us
             organized and focused. I can’t stop recommending them to everyone I
             talk to!”
@@ -53,7 +52,7 @@ export const Carousel = (props: Props) => {
         </div>
         <div id="avatarFour" className="carousel__item">
           <h3>Shanai Gough</h3>
-          <p>
+          <p className="general_text">
             “Their software allows us to track, manage and collaborate on our
             projects from anywhere. It keeps the whole team in-sync without
             being intrusive.”
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,30 +46,47 @@ export default function Home() {
             <div className="image__blob_two">
               <Image src={bgImage} height={400} width={400} alt="bg" />
             </div>
-            <h2>What’s different about Manage?</h2>
-            <p>
-              Manage provides all the functionality your team needs, without the
-              complexity. Our software is tailor-made for modern digital product
-              teams.
-            </p>
-            <h3>01 Track company-wide progress</h3>{" "}
-            <p>
-              See how your day-to-day tasks fit into the wider vision. Go from
-              tracking progress at the milestone level all the way done to the
-              smallest of details. Never lose sight of the bigger picture again.
-            </p>
-            <h3>02 Advanced built-in reports</h3>
-            <p>
-              Set internal delivery estimates and track progress toward company
-              goals. Our customisable dashboard helps you build out the reports
-              you need to keep key stakeholders informed.
-            </p>
-            <h3>03 Everything you need in one place</h3>{" "}
-            <p>
-              Stop jumping from one service to another to communicate, store
-              files, track tasks and share documents. Manage offers an
-              all-in-one team productivity solution.
-            </p>
+            {/* <section className="about">
+              <h2>What’s different about Manage?</h2>
+              <p>
+                Manage provides all the functionality your team needs, without
+                the complexity. Our software is tailor-made for modern digital
+                product teams.
+              </p>
+              <div>
+
+              </div>
+              <h3 className="flex f_center list">
+                <div className="flex f_center list__number">01</div> Track
+                company-wide progress
+              </h3>
+              <p>
+                See how your day-to-day tasks fit into the wider vision. Go from
+                tracking progress at the milestone level all the way done to the
+                smallest of details. Never lose sight of the bigger picture
+                again.
+              </p>
+              <div className="flex f_center">
+                <div className="list__number">02</div>
+                <h3 className="flex f_center list">
+                  Advanced built-in reports
+                </h3>
+              </div>
+              <p>
+                Set internal delivery estimates and track progress toward
+                company goals. Our customisable dashboard helps you build out
+                the reports you need to keep key stakeholders informed.
+              </p>
+              <h3 className="flex f_center list">
+                <div className="flex f_center list__number">03</div> Everything
+                you need in one place
+              </h3>{" "}
+              <p>
+                Stop jumping from one service to another to communicate, store
+                files, track tasks and share documents. Manage offers an
+                all-in-one team productivity solution.
+              </p>
+            </section> */}
           </section>
           <section>
             <Carousel />
